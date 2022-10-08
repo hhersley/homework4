e717b91f044f05ec9a08f7fcfef9f6d1e2f1c34e
hhersley <89473870+hhersley@users.noreply.github.com>
2022-10-07 22:11:56 -0500

    Update MenuItemADD.php

--- MenuItemADD.php
+++ MenuItemADD.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-
+?>
 
    <form method="post" action="MenuItemADDSAVE.php">
   <div class="mb-3">
@@ -36,30 +36,4 @@ if ($conn->connect_error) {
 </form>
    
 
-
-
-
-
-
-
-
-$mItem = $_POST['mItem'];
-$mPrice = $_POST['mPrice'];
-$mInfo = $_POST['mInfo'];
-
-
-$sql = "insert into Menu (menuitem) value (?)";
-//echo $sql;
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sis", $mItem, $mPrice, $mInfo);
-    $stmt->execute();
-?>
-    
-    <h1>Add Menu Item</h1>
-<div class="alert alert-success" role="alert">
-  New Menu Item Added successfuly!
-</div>
-    <a href="index.php" class="btn btn-primary">Go back</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
+</body>
