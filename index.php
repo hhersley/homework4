50a55118279e197cda4a2353f8eabcbdf0e156e2
hhersley <89473870+hhersley@users.noreply.github.com>
2022-10-07 21:43:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,54 @@
  <?php require_once('header.php'); ?>
 
 
-<h1>Homework4</h4>
+
+  <body>
+    
+    <h2>Menu</h2>
+    
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Food ID</th>
+      <th>Item</th>
+      <th>Price</th>
+     <th>Additional Information</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "hahersle_homework4";
+$password = "Hello10513!!";
+$dbname = "hahersle_homework4";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT FoodID, Item, Price, Info from Menu";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["FoodID"]?></td>
+    <td><?=$row["Item"]?></td>
+    <td><?=$row["Price"]?></td>
+    <td><?=$row["Info"]?></td>
+
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+      </tbody>
+    </table>
