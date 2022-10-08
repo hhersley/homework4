0432532624160764c6d66680c3a57e532dc8cf17
hhersley <89473870+hhersley@users.noreply.github.com>
2022-10-07 22:06:52 -0500

    Update MenuItemADD.php

--- MenuItemADD.php
+++ MenuItemADD.php
@@ -1,9 +1,12 @@
 <?php require_once('header.php'); ?>
 
+
+<body>
+  <?php 
 $servername = "localhost";
-$username = "hahersle_homework3";
+$username = "hahersle_homework4";
 $password = "Hello10513!!";
-$dbname = "hahersle_homework3";
+$dbname = "hahersle_homework4";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,6 +14,32 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+
+
+   <form method="post" action="MenuItemADD.php">
+  <div class="mb-3">
+    <label for="Item" class="form-label">Item</label>
+    <input type="text" class="form-control" id="item" aria-describedby="nameHelp" name="iName">
+    <div id="nameHelp" class="form-text">Enter the new item's name</div>
+   
+    <label for="Price" class="form-label">Price</label>
+    <input type="text" class="form-control" id="price" aria-describedby="nameHelp" name="iName">
+    <div id="nameHelp" class="form-text">Enter the new item's price</div>
+   
+    <label for="Info" class="form-label">Info</label>
+    <input type="text" class="form-control" id="info" aria-describedby="nameHelp" name="iName">
+    <div id="nameHelp" class="form-text">Enter any restrictions this fits (GF, Vegan, Vegetarian?</div>
+   
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+   
+
+
+
+
+
 
 
 
