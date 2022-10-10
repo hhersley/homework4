fd9f74096416f2a8526e0d12e84d5ecc12e1dae9
hhersley <89473870+hhersley@users.noreply.github.com>
2022-10-10 13:50:54 -0500

    Update GuestADDSAVE.php

--- GuestADDSAVE.php
+++ GuestADDSAVE.php
@@ -31,7 +31,7 @@ $sql = "insert into Guest (Name, Email) value (?, ?)";
 <div class="alert alert-success" role="alert">
   New Guest added successfuly!
 </div>
-    <a href="https://homework4.hahersley.oucreate.com/Guest,php" class="btn btn-primary">Go back</a>
+    <a href="https://homework4.hahersley.oucreate.com/Guest.php" class="btn btn-primary">Go back</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
