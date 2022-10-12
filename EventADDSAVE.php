e7ebc320a39f6d7b984c0b261764582118b59368
hhersley <89473870+hhersley@users.noreply.github.com>
2022-10-12 14:05:56 -0500

    Create EventADDSAVE.php

---
+++ EventADDSAVE.php
@@ -0,0 +1,36 @@
+<?php require_once('header.php'); ?>
+<body>
+  <?php
+$servername = "localhost";
+$username = "hahersle_homework4";
+$password = "Hello10513!!";
+$dbname = "hahersle_homework4";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+$eName = $_POST['eName'];
+  $eDate = $_POST['eDate'];
+$empID = $_POST['EmployeeID'];
+
+$sql = "insert into Event (Name, Date, EmployeeID) value (?, ?, ?)";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sdi", $eName, $eDate, $empID);
+    $stmt->execute();
+?>
+    
+    <h1>Add Event</h1>
+<div class="alert alert-success" role="alert">
+  New Event Added successfuly!
+</div>
+    <a href="https://homework4.hahersley.oucreate.com/Event.php" class="btn btn-primary">Go back</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
