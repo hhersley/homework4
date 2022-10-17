995635c418bd080d237c326a1b7376198c3e1f66
hhersley <89473870+hhersley@users.noreply.github.com>
2022-10-17 15:30:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
       <th>Item</th>
      <th>Price</th>
      <th>Event</th>
+     <th></th>
+     <th></th>
     
     </tr>
   </thead>
@@ -29,6 +31,28 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+   
+   
+   
+     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+
+    case 'Edit':
+      $sqlEdit = "update Menu set Item=?, Price=?, EventID=? where FoodID=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("ssii", $_POST['mItem'], $_POST['mPrice'], $_POST['eid'], $_POST['mid']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Menu item edited.</div>';
+      break;
+    case 'Delete':
+      $sqlDelete = "delete from Menu where FoodID=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("i", $_POST['mid']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">Menu item deleted.</div>';
+      break;
+  }
+}
 
 $sql = "SELECT FoodID, Item, Price, EName from Menu M Join Event E on M.EventID = E.EventID";
 $result = $conn->query($sql);
@@ -43,8 +67,82 @@ if ($result->num_rows > 0) {
        <td>$<?=$row["Price"]?></td>
           <td><?=$row["EName"]?></td>
 
+   
+    <td>
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editMenu<?=$row["FoodID"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editMenu<?=$row["FoodID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editMenu<?=$row["FoodID"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editMenu<?=$row["FoodID"]?>Label">Edit Menu</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editMenu<?=$row["EventID"]?>mItem" class="form-label">Item</label>
+                          <input type="text" class="form-control" id="editMenu<?=$row["FoodID"]?>Item" aria-describedby="editMenu<?=$row["FoodID"]?>Help" name="mItem" value="<?=$row['Item']?>">
+                          <div id="editMenu<?=$row["FoodID"]?>Help" class="form-text">Enter the item's name.</div>
+                        </div>
+                         
+ <div class="mb-3">
+                          <label for="editMenu<?=$row["EventID"]?>mPrice" class="form-label">Price</label>
+                          <input type="text" class="form-control" id="editMenu<?=$row["FoodID"]?>Price" aria-describedby="editMenu<?=$row["FoodID"]?>Help" name="mPrice" value="<?=$row['Price']?>">
+                          <div id="editMenu<?=$row["FoodID"]?>Help" class="form-text">Enter the item's Price.</div>
+                       
+                       
+                       </div>
+  <div class="mb-3">
+  <label for="EventList" class="form-label">Event</label>
+<select class="form-select" aria-label="Select Event" id="eventList" name="eid" value="<?=$row['EventID']?>">
+<?php
+    $eventSql = "select EName, EventID from Event order by EName";
+    $eventResult = $conn->query($eventSql);
+    while($eventRow = $eventResult->fetch_assoc()) {
+      if ($eventRow['EventID'] == $row['EventID']) {
+        $selText = " selected";
+      } else {
+        $selText = "";
+      }
+?>
+  <option value="<?=$eventRow['EventID']?>"<?=$selText?>><?=$eventRow['EName']?></option>
+<?php
+    }
+?>
+  
+  
+  
+        </select>
+  
+   
 
- 
+  </div>
+                      
+                        <input type="hidden" name="mid" value="<?=$row['FoodID']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+ <td>                    
+                     
+                  form method="post" action="">
+                <input type="hidden" name="mid" value="<?=$row["FoodID"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+              </form>
+            </td>
+
+  </tr>   
+                     
+                     
+                     
+                     
 
   </tr>
 <?php
@@ -57,6 +155,7 @@ $conn->close();
       </tbody>
     </table>
      <a href="https://homework4.hahersley.oucreate.com/MenuItemADD.php" class="btn btn-primary">New Item</a>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
    
 
