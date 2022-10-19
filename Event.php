62c13ab61b1ae852e48c3440b53f8886b0529499
hhersley <89473870+hhersley@users.noreply.github.com>
2022-10-18 22:15:52 -0500

    Update Event.php

--- Event.php
+++ Event.php
@@ -179,24 +179,21 @@ $conn->close();
      
      
        
-                      <div class="mb-3">
+                   
+                       <div class="mb-3">
                             <label for="EmployeeList" class="form-label">Employee</label>
                             <select class="form-select" aria-label="Select Employee" id="employeeList" name="eempid" value="<?=$row['EmployeeID']?>">
                           <?php
                               $eventSql = "select * from Employee order by Name";
                               $eventResult = $conn->query($eventSql);
                               while($eventRow = $eventResult->fetch_assoc()) {
-                               
-                                  $selText = "";
-                                
+                              
                          ?>
-                               <option value="<?=$eventRow['EmployeeID']?>"<?=$selText?>><?=$eventRow['Name']?></option>
+                               <option value="<?=$eventRow['EmployeeID']?>"</option>
                          <?php
                               }
                          ?>
                            </select>
-                       </div>
-     
    
                 <input type="hidden" name="saveType" value="Add">
                 <button type="submit" class="btn btn-primary">Submit</button>
